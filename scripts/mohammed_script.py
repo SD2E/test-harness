32a1062e3e04b4eeef0bfc8a7bd71a3c5af500bc
Mohammed Ali Eslami <meslami@netrias.com>
2019-01-26 10:30:12 -0500

    updated my script to match updated test harness

--- scripts/mohammed_script.py
+++ scripts/mohammed_script.py
@@ -1,22 +1,18 @@
-import argparse
-import datetime as dt
 import os
-import importlib
-import types
 import pandas as pd
-from tabulate import tabulate
 from pathlib import Path
 from sklearn.model_selection import train_test_split
-from test_harness_class import TestHarness
-from th_model_instances.hamed_models.random_forest_classification import random_forest_classification
+from test_harness.test_harness_class import TestHarness
+from test_harness.th_model_instances.hamed_models.random_forest_classification import random_forest_classification
+from test_harness.th_model_instances.hamed_models.random_forest_regression import random_forest_regression
 from pysd2cat.data import pipeline
 
 # SET PATH TO DATA FOLDER IN LOCALLY CLONED `versioned-datasets` REPO HERE:
 # Note that if you clone the `versioned-datasets` repo at the same level as where you cloned the `protein-design` repo,
 # then you can use VERSIONED_DATASETS = os.path.join(Path(__file__).resolve().parents[3], 'versioned-datasets/data')
-VERSIONED_DATA = os.path.join(Path(__file__).resolve().parents[3], 'versioned-datasets/data')
-print("Path to data folder in the locally cloned versioned-datasets repo was set to: {}".format(VERSIONED_DATA))
-print()
+#VERSIONED_DATA = os.path.join(Path(__file__).resolve().parents[3], 'versioned-datasets/data')
+#print("Path to data folder in the locally cloned versioned-datasets repo was set to: {}".format(VERSIONED_DATA))
+#print()
 
 PWD = os.getcwd()
 HERE = os.path.realpath(__file__)
@@ -37,9 +33,9 @@ def main():
 
     print("Length of full DF", len(df))
     input_cols = ['FSC-A', 'SSC-A', 'BL1-A', 'RL1-A', 'FSC-H', 'SSC-H', 'BL1-H', 'RL1-H', 'FSC-W', 'SSC-W', 'BL1-W', 'RL1-W']
-    output_cols = ["strain"]
+    output_cols = ["class_label"]
     print("Size of filtered data", len(df))
-    train, test = train_test_split(df, stratify=df['strain'], test_size=0.2, random_state=5)
+    train, test = train_test_split(df, stratify=df["class_label"], test_size=0.2, random_state=5)
     examples_folder_path = os.getcwd()
     print("initializing TestHarness object with output_location equal to {}".format(examples_folder_path))
     print()
@@ -47,7 +43,7 @@ def main():
 
     th.run_custom(function_that_returns_TH_model=random_forest_classification, dict_of_function_parameters={}, training_data=train,
                   testing_data=test, data_and_split_description="example custom run on Rocklin data",
-                  cols_to_predict='strain',
+                  cols_to_predict="class_label",
                   feature_cols_to_use=input_cols, normalize=True, feature_cols_to_normalize=input_cols,
                   feature_extraction=False, predict_untested_data=False)
 
