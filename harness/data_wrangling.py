1b364e55984e81de4533915415425b1c3f6ba06c
Jedediah Singer <jed@login1.maverick2.tacc.utexas.edu>
2019-02-15 15:34:30 -0600

    first commit, untested

--- harness/data_wrangling.py
+++ harness/data_wrangling.py
@@ -28,7 +28,8 @@ def encode_sequences(df, max_residues, col_to_encode='sequence', padding=14):
             range(23)))  # 'X' means nothing, 'J' means beginning, 'O' means end
 
     def make_code(sequence):
-        sequence = 'X' * padding + 'J' + (sequence + 'O').ljust(max_residues + 1 + padding, 'X')
+        extra_padding = hash(sequence) % (max_residues - len(sequence)) #MAX_RESIDUES doesn't include padding or J/O, so this is correct
+        sequence = ('X' * (padding + extra_padding) + 'J' + sequence + 'O').ljust(max_residues + 2 + 2*padding, 'X')
         code = np.zeros((23, len(sequence)))
         for i in range(len(sequence)):
             code[amino_dict[sequence[i]], i] = 1.0
@@ -39,3 +40,46 @@ def encode_sequences(df, max_residues, col_to_encode='sequence', padding=14):
     df = df.sample(frac=1)
 
     return df
+
+
+def encode_dssps(df, max_residues, col_to_encode='dssp', seq_col='sequence', padding=14):
+    structure_dict = dict(zip(['L', 'E', 'H', 'X', 'J', 'O'], range(6)))
+    def make_struct(dssp, sequence):
+        extra_padding = hash(sequence) % (max_residues - len(sequence))
+        dssp = ('X' * (padding + extra_padding) + 'J' + dssp + 'O').ljust(max_residues + 2 + 2*padding, 'X')
+        code = np.zeros((len(dssp), 6))
+        for i in range(len(dssp)):
+            code[i, structure_dict[dssp[i]]] = 1.0
+        return [code]
+    
+    df['encoded_dssp'] = df[col_to_encode].apply(lambda x: make_struct(x[col_to_encode], x[seq_col]))
+    # shuffle training data, because validation data are selected from end before shuffling:
+    df = df.sample(frac=1)
+    
+    return df
+    
+
+def encode_cnn_v2_targets(df,
+                          max_residues,
+                          stability_col='stabilityscore_cnn_calibrated',
+                          stability_c_col='stabilityscore_cnn_calibrated_c',
+                          stability_t_col='stabilityscore_cnn_calibrated_t',
+                          seq_col='sequence',
+                          dssp_col='dssp',
+                          padding=14
+                         ):
+    df = encode_dssps(df, max_residues, col_to_encode=dssp_col, seq_col=seq_col, padding=padding)
+    def make_target(row):
+        return (
+                (
+                 row[stability_c_col],
+                 row[stability_t_col],
+                 row[stability_col]
+                ),
+                row['encoded_dssp']
+               )
+    df = df.apply(make_target, axis=1)
+    
+    return df
+    
+    
