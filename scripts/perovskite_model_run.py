f949146e2a19cd6f9d5b93d334ec93837e7574f9
Hamed <eramian@netrias.com>
2019-02-27 16:58:01 -0500

    updated perovskites function to reflect crank format

--- scripts/perovskite_model_run.py
+++ scripts/perovskite_model_run.py
@@ -7,31 +7,38 @@ from harness.test_harness_class import TestHarness
 from harness.th_model_instances.hamed_models.random_forest_classification import random_forest_classification
 
 
-def initial_perovskites_run(df):
+def initial_perovskites_run(train_set, state_set):
     print("Starting test harness initial perovskites run")
-    all_cols = df.columns.tolist()
-    feature_cols = [c for c in all_cols if ("_rxn_" in c) or ("_feat_" in c)]
-    non_numerical_cols = (df.select_dtypes('object').columns.tolist())
+    all_cols = train_set.columns.tolist()
+    # don't worry about _calc_ columns for now, but it's in the code so they get included once the data is available
+    feature_cols = [c for c in all_cols if ("_rxn_" in c) or ("_feat_" in c) or ("_calc_" in c)]
+    non_numerical_cols = (train_set.select_dtypes('object').columns.tolist())
     feature_cols = [c for c in feature_cols if c not in non_numerical_cols]
+
+    # print(set(state_set.columns.tolist()).difference(set(feature_cols)))
+    # print(set(feature_cols).difference(set(state_set.columns.tolist())))
+    # remove _rxn_temperatureC_actual_bulk column from feature_cols because it doesn't exist in state_set
+    feature_cols.remove("_rxn_temperatureC_actual_bulk")
+
+
 
     # create binarized crystal scores because Ian said to start with binary task
     # also multiclass support needs to be added to Test Harness
     conditions = [
-        (df['_out_crystalscore'] == 1),
-        (df['_out_crystalscore'] == 2),
-        (df['_out_crystalscore'] == 3),
-        (df['_out_crystalscore'] == 4),
+        (train_set['_out_crystalscore'] == 1),
+        (train_set['_out_crystalscore'] == 2),
+        (train_set['_out_crystalscore'] == 3),
+        (train_set['_out_crystalscore'] == 4),
     ]
     binarized_labels = [0, 0, 0, 1]
-    df['binarized_crystalscore'] = np.select(conditions, binarized_labels)
-    col_order = list(df.columns.values)
+    train_set['binarized_crystalscore'] = np.select(conditions, binarized_labels)
+    col_order = list(train_set.columns.values)
     col_order.insert(3, col_order.pop(col_order.index('binarized_crystalscore')))
-    df = df[col_order]
-    print(df)
+    train_set = train_set[col_order]
 
     col_to_predict = 'binarized_crystalscore'
 
-    train, test = train_test_split(df, test_size=0.2, random_state=5, stratify=df[['dataset']])
+    train, test = train_test_split(train_set, test_size=0.2, random_state=5, stratify=train_set[['dataset']])
 
     # Test Harness use starts here:
     current_path = os.getcwd()
@@ -43,7 +50,7 @@ def initial_perovskites_run(df):
                   testing_data=test, data_and_split_description="test run on perovskite data",
                   cols_to_predict=col_to_predict,
                   feature_cols_to_use=feature_cols, normalize=True, feature_cols_to_normalize=feature_cols,
-                  feature_extraction=False, predict_untested_data=False)
+                  feature_extraction=False, predict_untested_data=state_set)
 
 
 if __name__ == '__main__':
@@ -51,12 +58,13 @@ if __name__ == '__main__':
     print("Path to data folder in the locally cloned versioned-datasets repo was set to: {}".format(VERSIONED_DATA))
     assert os.path.isdir(VERSIONED_DATA), "The path you gave for VERSIONED_DATA does not exist."
 
-
     # Reading in data from versioned-datasets repo.
-    df = pd.read_csv(
-        os.path.join(VERSIONED_DATA, 'perovskite/experimental_run/2017-07-20-Final.experimental_run.csv'),
-        comment='#',
-        low_memory=False)
+    df = pd.read_csv(os.path.join(VERSIONED_DATA, 'perovskite/perovskitedata/0018.perovskitedata.csv'),
+                     comment='#',
+                     low_memory=False)
 
-    initial_perovskites_run(df)
+    state_set = pd.read_csv(os.path.join(VERSIONED_DATA, 'perovskite/stateset/0018.stateset.csv'),
+                            comment='#',
+                            low_memory=False)
 
+    initial_perovskites_run(df, state_set)
