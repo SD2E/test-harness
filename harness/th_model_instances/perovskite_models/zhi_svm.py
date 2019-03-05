5016a9e791824afa8e56029b4d8390c163a4fa1e
Nick Leiby <nick.leiby@twosixlabs.com>
2019-03-05 11:24:52 -0500

    Adds the SVM that Jed and Zhi worked on

---
+++ harness/th_model_instances/perovskite_models/zhi_svm.py
@@ -0,0 +1,17 @@
+from sklearn.svm import SVC
+from harness.th_model_classes.class_sklearn_classification import SklearnClassification
+
+
+def support_vector_radial_basis_classifier():
+    # RBF SVM hyperparameters optimized by Zhi Li for perovskite data classification
+    support_vector_model = SVC(C=100000,
+                               gamma=0.1,
+                               cache_size=5000,
+                               max_iter=-1,
+                               kernel='rbf',
+                               decision_function_shape='ovr',
+                               probability=True
+                               )
+    th_model = SklearnClassification(model=support_vector_model,
+                                     model_description="svm with radial basis function")
+    return th_model
