25d0d29959d71fc436c2438bdfb91d2584c99034
Hamed <eramian@netrias.com>
2019-03-27 09:18:18 -0400

    updated perovskite scripts to use baseline models

--- scripts_for_automation/perovskite_models_config.py
+++ scripts_for_automation/perovskite_models_config.py
@@ -1,10 +1,14 @@
 from harness.th_model_instances.hamed_models.random_forest_classification import random_forest_classification
 from harness.th_model_instances.hamed_models.weighted_logistic import weighted_logistic_classifier
 from harness.th_model_instances.perovskite_models.zhi_svm import support_vector_radial_basis_classifier
+from harness.th_model_instances.baseline_models.classification_baselines import baseline_prior, baseline_stratified, baseline_uniform
 
 MODELS_TO_RUN = \
     [
-        support_vector_radial_basis_classifier,
-        random_forest_classification,
-        weighted_logistic_classifier
+        baseline_prior,
+        # baseline_stratified,
+        # baseline_uniform,
+        # weighted_logistic_classifier,
+        # random_forest_classification,
+        # support_vector_radial_basis_classifier
     ]
