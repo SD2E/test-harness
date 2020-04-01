81402bd99afde490c3fc8d5facbf39076aaa4e86
Mohammed Ali Eslami <meslami@netrias.com>
2020-04-01 08:17:49 -0400

    updated to include additional parsing capability and a naive bayes model.

--- harness/th_model_instances/hamed_models/navie_bayes.py
+++ harness/th_model_instances/hamed_models/navie_bayes.py
@@ -2,8 +2,17 @@ from sklearn.naive_bayes import GaussianNB
 from harness.th_model_classes.class_sklearn_classification import SklearnClassification
 
 
-def gaussan_naive_bayes_classification(n_estimators=361, max_features='auto', criterion='entropy', min_samples_leaf=13,
-                                 n_jobs=-1, class_weight="balanced"):
+def gaussian_naive_bayes_classification():
+    '''
+
+    :param n_estimators:
+    :param max_features:
+    :param criterion:
+    :param min_samples_leaf:
+    :param n_jobs:
+    :param class_weight:
+    :return:
+    '''
     # Creating an sklearn random forest classification model:
     gnb = GaussianNB()
 
--- harness/utils/parsing_results.py
+++ harness/utils/parsing_results.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import sklearn.metrics as metrics
+
 
 
 from harness.utils.names import Names
@@ -34,10 +36,10 @@ def get_leaderboard(th_output_location=None, loo=True, classification=True):
     return leaderboard
 
 
-def get_result_csvs(loo_or_run_ids, th_output_location=None, file_type=Names.TESTING_DATA):
+def get_result_csv_paths(loo_or_run_ids, th_output_location=None, file_type=Names.TESTING_DATA):
     '''
     Get result csv paths
-    :param loo_or_run_ids: if a loo run, put the id of the loo run as key and the run_ids of that loo as a value
+    :param loo_or_run_ids: if a loo run, put the id of the loo run as key and the run_ids of that loo as a value list
     if just a custom_run, make it a list
     :param th_output_location: Location of you gave to the test harness
     :param file_type: must be in Names.OUTPUT_FILE keys
@@ -65,7 +67,7 @@ def get_result_csvs(loo_or_run_ids, th_output_location=None, file_type=Names.TES
 
         else:
             if type(loo_or_run_ids) == list:
-                output_csv_path = os.path.join(th_output_location,'test_harness_results','runs','run_'+run_id,Names.OUTPUT_FILES[file_type])
+                output_csv_path = os.path.join(th_output_location,'test_harness_results','runs','run_'+item,Names.OUTPUT_FILES[file_type])
                 output_csv_paths.append(output_csv_path)
             else:
                 for run_id in loo_or_run_ids[item]:
@@ -73,6 +75,134 @@ def get_result_csvs(loo_or_run_ids, th_output_location=None, file_type=Names.TES
                                                        Names.OUTPUT_FILES[file_type])
                     output_csv_paths.append(output_csv_path)
     return output_csv_paths
+
+
+
+def get_incorrect_classification_results_query(query, th_output_location, loo=False, file_type=Names.TESTING_DATA):
+    '''
+    This method queries the leaderboard, retrieves the files of the associated run_ids, subsets that
+    :param query: query to subset the leaderboard
+    :param th_output_location: path to output location
+    :param loo: Boolean. If a leave one out runn.
+    :param classification: Boolean. True if classification
+    :param file_type: one of the output of the files
+    :return:
+    '''
+    df_leaderboard_sub = query_leaderboard(query=query,th_output_location=th_output_location,loo=loo,classification=True)
+    col_to_predict = df_leaderboard_sub[Names.COLUMN_PREDICTED].unique()
+    if len(col_to_predict)>1:
+        raise RuntimeError('This function can only be used when you have a single predicted column. You currently have {0}'.format(len(col_to_predict)))
+    print("Column to predict",col_to_predict)
+
+    paths = get_result_csv_paths_query(query=query,th_output_location=th_output_location,loo=loo,file_type=file_type,classification=True)
+    dfs = []
+    for path in paths:
+        df = pd.read_csv(path)
+        df = df.loc[~(df[col_to_predict[0]] == df[col_to_predict[0]+'_predictions'])]
+        df['path']=path
+        dfs.append(df)
+    df_all = pd.concat(dfs)
+    return df_all
+
+def get_correct_classification_results_query(query, th_output_location, loo=False, file_type=Names.TESTING_DATA):
+    '''
+    This method queries the leaderboard, retrieves the files of the associated run_ids, subsets that
+    :param query: query to subset the leaderboard
+    :param th_output_location: path to output location
+    :param loo: Boolean. If a leave one out runn.
+    :param classification: Boolean. True if classification
+    :param file_type: one of the output of the files
+    :return:
+    '''
+    df_leaderboard_sub = query_leaderboard(query=query,th_output_location=th_output_location,loo=loo,classification=True)
+    col_to_predict = df_leaderboard_sub[Names.COLUMN_PREDICTED].unique()
+    if len(col_to_predict)>1:
+        raise RuntimeError('This function can only be used when you have a single predicted column. You currently have {0}'.format(len(col_to_predict)))
+    print("Column to predict",col_to_predict)
+
+    paths = get_result_csv_paths_query(query=query,th_output_location=th_output_location,loo=loo,file_type=file_type,classification=True)
+    dfs = []
+    for path in paths:
+        df = pd.read_csv(path)
+        df = df.loc[(df[col_to_predict[0]] == df[col_to_predict[0]+'_predictions'])]
+        df['path']=path
+        dfs.append(df)
+    df_all = pd.concat(dfs)
+    return df_all
+
+def get_roc_curve_query(query, th_output_location, loo=False, file_type=Names.TESTING_DATA):
+    '''
+    This method queries the leaderboard, retrieves the files of the associated run_ids, subsets that
+    :param query: query to subset the leaderboard
+    :param th_output_location: path to output location
+    :param loo: Boolean. If a leave one out run.
+    :param classification: Boolean. True if classification
+    :param file_type: one of the output of the files
+    :return:
+    '''
+    df_leaderboard_sub = query_leaderboard(query=query,th_output_location=th_output_location,loo=loo,classification=True)
+    col_to_predict = df_leaderboard_sub[Names.COLUMN_PREDICTED].unique()
+    if len(col_to_predict)>1:
+        raise RuntimeError('This function can only be used when you have a single predicted column. You currently have {0}'.format(len(col_to_predict)))
+    print("Column to predict",col_to_predict)
+    paths = get_result_csv_paths_query(query=query,th_output_location=th_output_location,loo=loo,file_type=file_type,classification=True)
+    dfs = []
+    for path in paths:
+        df_preds = pd.read_csv(path)
+        y_true = df_preds[col_to_predict[0]].tolist()
+        y_probas = df_preds[col_to_predict[0]+'_prob_predictions'].tolist()
+        fpr, tpr, threshold = metrics.roc_curve(y_true, y_probas)
+        ###TODO: NEED TO FIGURE OUT WHAT TO DO WITH THIS!
+
+def get_incorrect_classification_results(loo_or_run_ids, th_output_location, loo=False, file_type=Names.TESTING_DATA):
+    '''
+    This method queries the leaderboard, retrieves the files of the associated run_ids, subsets that
+    :param loo_or_run_ids: dictionary of loo/run ids
+    :param th_output_location: path to output location
+    :param loo: Boolean. If a leave one out runn.
+    :param classification: Boolean. True if classification
+    :param file_type: one of the output of the files
+    :return: the
+    '''
+    df_leaderboard_list = []
+    for item in loo_or_run_ids:
+        query={}
+        if loo:
+            query[Names.LOO_ID]=item
+            query[Names.RUN_ID]=loo_or_run_ids[item]
+        else:
+            query[Names.RUN_ID]=item
+        df_sub = get_incorrect_classification_results_query(query=query,th_output_location=th_output_location,loo=loo, \
+                                                            file_type=file_type)
+        df_leaderboard_list.append(df_sub)
+    df_all = pd.concat(df_leaderboard_list)
+    return df_all
+
+
+def get_result_csv_paths_query(query, th_output_location, loo=False, classification=False,file_type=Names.TESTING_DATA):
+    '''
+    Get the results of a leaderboard from a query
+    :param query: query
+    :param th_output_location: test harness location
+    :param loo: Boolean of whether you are querying a loo run
+    :param classification: Boolean of whether this was a classification test
+    :param file_type: the file type you want to return
+    :return: list of paths associated with the query
+    '''
+    df_sub = query_leaderboard(query, th_output_location, loo=loo, classification=classification)
+    if loo:
+        loo_or_run_ids={}
+        for ind in df_sub.index:
+            loo_id = df_sub.loc[ind,Names.LOO_ID]
+            run_id = df_sub.loc[ind,Names.RUN_ID]
+            if loo_id not in loo_or_run_ids:
+                loo_or_run_ids[loo_id]=[]
+            loo_or_run_ids[loo_id].append(run_id)
+    else:
+        loo_or_run_ids = df_sub[Names.RUN_ID].tolist()
+    paths = get_result_csv_paths(loo_or_run_ids=loo_or_run_ids,th_output_location=th_output_location,file_type=file_type)
+    return paths
+
 
 
 def query_leaderboard(query, th_output_location, loo=False, classification=False):
@@ -100,21 +230,15 @@ def query_leaderboard(query, th_output_location, loo=False, classification=False
 
 
 if __name__ == '__main__':
+    th_location = '/Users/meslami/Documents/GitRepos/test-harness/example_scripts/Data_Sharing_Demo'
 
-    ##LOO Test
-    th_location = '/Users/meslami/Documents/GitRepos/sd2-chaos/data/NovelChassis/NAND_20_Outputs/outputs_loo_part_condition'
-    ##Example use of collecting test or predicted data
-    paths = get_result_csvs(loo_or_run_ids={'52x6jbP3XX8AN':['dRb2j2jROwAQo','aPVG7Ray7mpQk','ml5YY9XNrrGwW']},th_output_location=th_location)
-    print(paths)
+    # Example of use of query to subset leaderboard
+    query = {Names.MODEL_NAME:'random_forest',Names.TEST_GROUP:"topology: EHEE"}
+    sub_df = query_leaderboard(query, th_location, loo=True,classification=True)
 
-    #Example of use of query
-    query = {Names.MODEL_NAME:'random_forest',Names.TEST_GROUP:"timepoint: 5"}
-    sub_df = query_leaderboard(query, th_location, classification=True)
-    print(sub_df.head(3))
+    # Example of use of query to get incorrect results
+    query = {Names.MODEL_NAME: 'random_forest', Names.TEST_GROUP: "topology: EHEE"}
+    sub_df = get_incorrect_classification_results_query(query, th_location, loo=True)
+    print(sub_df.head)
 
-    ##Custom Run Test
-    th_location = None
-    run_list = ['5dDzoa5ENJLyM','5Mr2mRqVbNAZN']
-    paths = get_result_csvs(loo_or_run_ids=run_list)
-    print(paths)
 
