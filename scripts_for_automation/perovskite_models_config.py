f62267495bb67d79be1299c4bd94eeb2148fde2a
Nick Leiby <nick.leiby@twosixlabs.com>
2020-04-13 18:48:07 -0400

    Fix the turned off model...

--- scripts_for_automation/perovskite_models_config.py
+++ scripts_for_automation/perovskite_models_config.py
@@ -12,8 +12,8 @@ from harness.th_model_instances.perovskite_models.rxn_ratio_only import rxn_intu
 MODELS_TO_RUN = [
         baseline_uniform,
         weighted_logistic_classifier,
-        rxn_only_svm,
-        # rxn_intuition_svm,
+        # rxn_only_svm,
+        rxn_intuition_svm,
         random_forest_classification,
         gradient_boosted_tree,
         support_vector_radial_basis_classifier,
